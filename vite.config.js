99446964a2fb132f7d4ac878cd629f88ffeb42ee
Liciaav <liciaavasc@gmail.com>
2025-01-14 14:40:21 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    open: true
+  root: './',  
+  build: {
+    rollupOptions: {
+      input: './index.html',  
+    },
   },
-  build:{
-    
-  }
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
+
