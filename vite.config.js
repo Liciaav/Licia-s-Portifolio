182d7db8a05b4af57ce98fefb181b65a009b9d4d
Liciaav <liciaavasc@gmail.com>
2025-01-14 13:25:38 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,5 @@ export default defineConfig({
   server:{
     port:3000,
     open: true
-  },
-  build: {
-    rollupOptions: {
-      external: [],  // Desabilitar a externalização
-    }
   }
 })
