04c1d616e7d942df64b50f936579ecfe13a69f03
Liciaav <liciaavasc@gmail.com>
2025-01-15 10:33:00 -0500

    update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
         <p className="resume-description">
           Download my resume by clicking the button below:
         </p>
-        <a target="_blank" rel="noopener noreferrer" href="https://1drv.ms/w/c/2067ba7f6a253d08/EU9_HmmYMY1Br6cfC-srVI4B0PnTGqO_zsTVpI_v7f5Utg?e=jcRRhp&download=1" download className="resume-button">
+        <a target="_blank" rel="noopener noreferrer" href="https://onedrive.live.com/download?cid=2067ba7f6a253d08&resid=2067BA7F6A253D08!100&authkey=!EU9_HmmYMY1Br6cfC-srVI4B0PnTGqO_zsTVpI_v7f5Utg" download className="resume-button">
           Download Resume
         </a>
       </section>
