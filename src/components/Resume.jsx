a2fe7d631f1b5d1b15495ebf5c015b795bab69c4
Liciaav <liciaavasc@gmail.com>
2025-01-15 15:35:27 -0500

    update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,18 +3,34 @@ import Layout from "./Layout.jsx";
 import './Resume.css'; 
 
 function Resume() {
+  const fileUrl = "https://1drv.ms/w/c/2067ba7f6a253d08/IQRPfx5pmDGNQa-nHwvrK1SOAQRcY2ziDT-3Mj_wfU6SLQ4";
+
   return (
     <Layout>
       <section className="resume-container">
+        <h2 className="resume-title">Resume</h2>
         <p className="resume-description">
-          Download my resume by clicking the button below:
+          You can view or download my resume by clicking the buttons below:
         </p>
-        <a target="_blank" rel="noopener noreferrer" href="https://onedrive.live.com/download?cid=2067ba7f6a253d08&resid=IQRPfx5pmDGNQa-nHwvrK1SOAQRcY2ziDT-3Mj_wfU6SLQ4&authkey=AUTHKEY" download className="resume-button">
-          Download Resume
-        </a>
+        <div className="resume-buttons">
+          <a 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            href="https://1drv.ms/w/c/2067ba7f6a253d08/EU9_HmmYMY1Br6cfC-srVI4B0PnTGqO_zsTVpI_v7f5Utg?e=jNsKhf" 
+            className="resume-button">
+            View Resume
+          </a>
+          <a 
+            href={fileUrl} 
+            download 
+            className="resume-button">
+            Download Resume
+          </a>
+        </div>
       </section>
     </Layout>
   );
 }
 
 export default Resume;
+
