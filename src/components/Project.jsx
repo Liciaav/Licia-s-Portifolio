0262e764e097af70915d97c7756de21dc5262e64
Liciaav <liciaavasc@gmail.com>
2025-01-15 20:53:33 -0500

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,57 +4,57 @@ function Project() {
   const projects = [
     {
       title: "READme",
-      imageUrl: "./src/image/readme.webp",
+      imageUrl: "/images/readme.webp",
       githubLink: "https://github.com/Liciaav/Licia-README",
     },
     {
       title: "My Cars",
-      imageUrl: "./src/image/cars.jpg",
+      imageUrl: "/images/cars.jpg",
       githubLink: "https://github.com/Liciaav/Licia-My-Cars",
     },
     {
       title: "Weather App",
-      imageUrl: "./src/image/wheather.jpg",
+      imageUrl: "/images/wheather.jpg",
       githubLink: "https://github.com/Liciaav/Licia-Weather.app",
     },
     {
       title: "CMS",
-      imageUrl: "./src/image/emplotee.avif",
+      imageUrl: "/images/employee.avif",
       githubLink: "https://github.com/Liciaav/CMS-project",
     },
     {
       title: "Candidate Search",
-      imageUrl: "./src/image/candidate-search.jpg",
+      imageUrl: "/images/candidate-search.jpg",
       githubLink: "https://github.com/Liciaav/Licia-s-Candidate-Search",
     },
     {
       title: "Kanban Board",
-      imageUrl: "./src/image/kanban-board.avif",
+      imageUrl: "/images/kanban-board.avif",
       githubLink: "https://github.com/Liciaav/Licia-Kanban-Board",
     },
     {
       title: "Social Network",
-      imageUrl: "./src/image/social-network.jpg",
+      imageUrl: "/images/social-network.jpg",
       githubLink: "https://github.com/Liciaav/Licia-Social-Network",
     },
     {
       title: "Book Search",
-      imageUrl: "./src/image/book-search.jpg",
+      imageUrl: "/images/book-search.jpg",
       githubLink: "https://github.com/Liciaav/Licia-Book-Search",
     },
     {
       title: "Happiest of Hours",
-      imageUrl: "./src/image/happy-hour.jpg",
+      imageUrl: "/images/happy-hour.jpg",
       githubLink: "https://github.com/BriannaVitan/Happiest-of-Hours",
     },
     {
       title: "Trip Planner 3000",
-      imageUrl: "./src/image/viagem.webp",
+      imageUrl: "/images/viagem.webp",
       githubLink: "https://github.com/kaneganteng/Trip-Planner-3000",
     },
     {
       title: "Hangman 2",
-      imageUrl: "./src/image/hangman.webp",
+      imageUrl: "/images/hangman.webp",
       githubLink: "https://github.com/IsakTL/hangman-2",
     },
   ];
